1a9b2150445f753b37e17973592df63784ff4d94
Angel <3555772080101@ingenieria.usac.edu.gt>
2024-02-08 10:23:54 -0600

    Tomar fotos en frontend

---
+++ Tareas/TAREA2/frontend/tarea2/src/App.js
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from 'react';
+
+function App() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [base64Image, setBase64Image] = useState(null);
+
+  const startCamera = async () => {
+    const constraints = { video: true };
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoRef.current.srcObject = stream;
+    videoRef.current.play();
+  };
+
+  const takePhoto = () => {
+    const context = canvasRef.current.getContext('2d');
+    context.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
+    const base64String = canvasRef.current.toDataURL('image/png').split(",")[1];
+    setBase64Image(base64String);
+    
+    
+  };
+
+  return (
+    <div className="App">
+      <center>
+        <div className='video-container'>
+          <video className='video' ref={videoRef} width="640" height="480" />
+          <canvas className='canvas' ref={canvasRef} width="640" height="480" />
+        </div>
+        <br></br>
+        <button className='camara-btn' onClick={startCamera}>Iniciar cámara</button>
+        <button className='foto-btn' onClick={takePhoto}>Tomar foto</button>
+      </center>
+    </div>
+  );
+}
+
+export default App;
