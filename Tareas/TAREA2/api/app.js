5da3254f8e07a9f5d8699ac70fcac5f8a3f69b17
Angel Sique <3555772080101@ingenieria.usac.edu.gt>
2024-02-09 00:04:21 -0600

    Docker compose

--- Tareas/TAREA2/api/app.js
+++ Tareas/TAREA2/api/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect(`mongodb://MongoDB:27017/tarea2-db`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conexión a MongoDB exitosa'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
+
+// Define el modelo de imagen
+const Foto = mongoose.model('Foto', new mongoose.Schema({
+    image: String
+}), 'fotos');
 
 app.use(cors()); // Usa CORS en todas las rutas
 app.use(express.json({ limit: '50mb' })); // Aumenta el límite a 50MB
 
 app.post('/upload', (req, res) => {
     const base64Image = req.body.image;
-    // Aquí puedes procesar la imagen en base64 como desees
-    // Por ejemplo, puedes guardarla en el sistema de archivos o enviarla a un servicio externo
-    
-    // Envía una respuesta de éxito
-    res.status(200).json({ message: 'Imagen recibida correctamente' });
+
+    // Crea una nueva imagen y la guarda en la base de datos
+    const foto = new Foto({ image: base64Image });
+    foto.save()
+        .then(() => res.status(200).json({ message: 'Imagen recibida y guardada correctamente' }))
+        .catch(err => res.status(500).json({ message: 'Error al guardar la imagen', error: err }));
 });
 
 app.listen(8080, () => {
-    console.log('Servidor escuchando en el puerto 8080');
+    console.log(`Servidor escuchando en 8080`);
 });
