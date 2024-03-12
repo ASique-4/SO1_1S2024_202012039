1619b74f5e14ac3c159dd8501d05f5ab2dadbd45
Angel Sique <3555772080101@ingenieria.usac.edu.gt>
2024-03-12 11:00:00 -0600

    Página home en react

---
+++ PROYECTO1/FRONTEND/frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <section className="home-section">
+                <div className="home-content">
+                    <h1 className="home-title">Proyecto 1 - SO1</h1>
+                    <p className="home-text">Hecho por Angel Sique, Carnet: 202012039</p>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
