b38f8c1f3a4cf019cda35c7abfc896b144a45abe
ASique-4 <3555772080101@ingenieria.usac.edu.gt>
2024-03-17 14:51:18 -0600

    obtener datos de la ram

--- PROYECTO1/BACKEND/main.go
+++ PROYECTO1/BACKEND/main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 // Datos de la RAM
@@ -59,34 +60,42 @@ func saveRamData(w http.ResponseWriter, r *http.Request) {
 
 // Función para manejar las solicitudes HTTP GET a /ram
 func getRamData(w http.ResponseWriter, r *http.Request) {
-	// Consultar los datos de la tabla MemoriaRAM
-	rows, err := db.Query("SELECT totalRam, memoriaEnUso, porcentaje, libre, fechaHora FROM MemoriaRAM")
+	// Leer los datos de /proc/ram_so1_1s2024
+	file, err := os.Open("/proc/ram_so1_1s2024")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer rows.Close()
+	defer file.Close()
 
-	// Leer los resultados de la consulta
-	var datos []RamData
-	for rows.Next() {
-		var dato RamData
-		err := rows.Scan(&dato.Total, &dato.EnUso, &dato.Porcentaje, &dato.Libre, &dato.FechaHora)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		datos = append(datos, dato)
+	var ram RamData
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&ram)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	// Comprobar si hubo errores al leer los resultados
-	if err := rows.Err(); err != nil {
+	// Guardar los datos en la base de datos
+	now := time.Now().Format("2006-01-02 15:04:05")
+	_, err = db.Exec("INSERT INTO MemoriaRAM (total, enUso, porcentaje, libre, fechaHora) VALUES (?, ?, ?, ?, ?)", ram.Total, ram.EnUso, ram.Porcentaje, ram.Libre, now)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Consultar el último dato ingresado en la tabla MemoriaRAM
+	row := db.QueryRow("SELECT total, enUso, porcentaje, libre, fechaHora FROM MemoriaRAM ORDER BY fechaHora DESC LIMIT 1")
+
+	var dato RamData
+	err = row.Scan(&dato.Total, &dato.EnUso, &dato.Porcentaje, &dato.Libre, &dato.FechaHora)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// Convertir los datos a JSON
-	jsonData, err := json.Marshal(datos)
+	jsonData, err := json.Marshal(dato)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -136,7 +145,14 @@ func main() {
 	// Manejar la ruta GET para obtener datos de RAM
 	router.HandleFunc("/ram", getRamData).Methods("GET")
 
-	// Iniciar el servidor HTTP
+	// Configurar CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},           // Permitir todas las origenes
+		AllowedMethods: []string{"GET", "POST"}, // Permitir métodos GET y POST
+		AllowedHeaders: []string{"*"},           // Permitir todas las cabeceras
+	})
+
+	// Iniciar el servidor HTTP con CORS habilitado
 	log.Println("Servidor iniciado en el puerto 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", c.Handler(router)))
 }
